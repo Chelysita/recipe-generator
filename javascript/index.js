01920e8a8e96cb495b9f2259eacc23d28ef37a98
Chelysita <ara.medinago@gmail.com>
2025-05-17 13:49:17 -0400

    Basic JS added with fake information

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "The recipe will appear here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#formElement");
+formElement.addEventListener("submit", generatePoem);
