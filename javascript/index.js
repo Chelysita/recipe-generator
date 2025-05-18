27391e5aa62c175866c7a96c99a0388afb6e6991
Chelysita <ara.medinago@gmail.com>
2025-05-18 14:08:39 -0400

    Added some user experience

--- javascript/index.js
+++ javascript/index.js
@@ -12,11 +12,11 @@ function generateRecipe(event) {
   let userElement = document.querySelector("#user-instructions");
   let apikey = "e1054aod00e03d00aeectb39af13a2bb";
   let context = "You are a chef that know easy and fast recipes";
-  let prompt = `Provide a recipe that includes ${userElement.value} as one of the ingredients, separate with a line and capitalize and bold the title of the recipe. Separate each line and the ingredients with a <br/> element`;
+  let prompt = `Provide a recipe that includes ${userElement.value} as one of the ingredients, for the title of the recipe:separate with a <br/>, capitalize and bold. SEPARATE ingredients and each line with a <br/> element`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
   axios.get(apiUrl).then(showRecipe);
   let generatingElement = document.querySelector("#recipe");
-  generatingElement.innerHTML = `Generating recipe that includes <strong>${userElement.value} </strong> as an ingredient`;
+  generatingElement.innerHTML = `<div class=blink>😃 Generating recipe that includes <strong>${userElement.value} </strong> as an ingredient</div>`;
 }
 
 let formElement = document.querySelector("#formElement");
